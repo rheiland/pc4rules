e185340a323f4b0cd4a1e36d877ba3f54175145d
Randy Heiland <randy.heiland@gmail.com>
2023-01-26 18:56:21 -0500

    About tab on startup

--- bin/studio.py
+++ bin/studio.py
@@ -343,7 +343,7 @@ class PhysiCellXMLCreator(QWidget):
             self.tabWidget.setCurrentIndex(0)  # About
 
         # self.tabWidget.setCurrentIndex(5)  # rwh hack/debug/default
-        self.tabWidget.setCurrentIndex(7)  # Run
+        # self.tabWidget.setCurrentIndex(7)  # Run
         # self.tabWidget.setCurrentIndex(8)  # Plot
 
 
