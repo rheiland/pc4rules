536307e0818a28b5591fabdaec61841b3bf62219
Randy Heiland <randy.heiland@gmail.com>
2023-05-12 20:42:41 -0400

    update

--- bin/studio.py
+++ bin/studio.py
@@ -319,6 +319,16 @@ class PhysiCellXMLCreator(QWidget):
 
             self.homedir = os.getcwd()
             print("studio.py: self.homedir = ",self.homedir)
+            if self.nanohub_flag:
+                try:
+                    # cachedir = os.environ['CACHEDIR']
+                    toolpath = os.environ['TOOLPATH']
+                    print("studio.py: toolpath= ",toolpath)
+                    # full_path = os.path.join(toolpath, "data")
+                    self.homedir = os.path.join(toolpath, "data")
+                except:
+                    print("studio.py: exception doing os.environ('TOOLPATH')")
+
             self.run_tab.homedir = self.homedir
 
             # self.run_tab.config_xml_name.setText(current_xml_file)
@@ -1152,7 +1162,7 @@ def main():
         parser.add_argument("-b", "--bare", "--basic", help="no plotting, etc ", action="store_true")
         parser.add_argument("-3", "--three", "--3D", help="assume a 3D model", action="store_true")
         parser.add_argument("-t", "--tensor",  help="for 3D ellipsoid cells", action="store_true")
-        parser.add_argument("-r", "--rules", "--Rules", help="display Rules tab" , action="store_true")
+        # parser.add_argument("-r", "--rules", "--Rules", help="display Rules tab" , action="store_true")
         parser.add_argument("-x", "--skip_validate", help="do not attempt to validate the config (.xml) file" , action="store_true")
         parser.add_argument("--nanohub", help="run as if on nanoHUB", action="store_true")
         parser.add_argument("--is_movable", help="checkbox for mechanics is_movable", action="store_true")
@@ -1163,10 +1173,17 @@ def main():
 
         # args = parser.parse_args()
         args, unknown = parser.parse_known_args()
+        print("args=",args)
+        print("unknown=",unknown)
         if unknown:
-            print("invalid argument: ",unknown)
-            sys.exit(-1)
+            if unknown[0] == "--rules":
+                print("studio.py: setting rules_flag = True")
+                rules_flag = True
+            else:
+                print("invalid argument: ",unknown)
+                sys.exit(-1)
 
+        print("-- continue after if unknown...")
         if args.three:
             logging.debug(f'studio.py: Assume a 3D model')
             model3D_flag = True
@@ -1179,9 +1196,9 @@ def main():
             studio_flag = False
             model3D_flag = False
             # print("done with args.studio")
-        if args.rules:
-            logging.debug(f'studio.py: Show Rules tab')
-            rules_flag = True
+        # if args.rules:
+        #     logging.debug(f'studio.py: Show Rules tab')
+        #     rules_flag = True
         if args.nanohub:
             logging.debug(f'studio.py: nanoHUB mode')
             nanohub_flag = True
@@ -1277,6 +1294,7 @@ def main():
             rules_flag = False
             print("Warning: Rules module not found.\n")
 
+    # print("calling PhysiCellXMLCreator with rules_flag= ",rules_flag)
     ex = PhysiCellXMLCreator(config_file, studio_flag, skip_validate_flag, rules_flag, model3D_flag, tensor_flag, exec_file, nanohub_flag, is_movable_flag)
     print("size=",ex.size())  # = PyQt5.QtCore.QSize(1100, 770)
     # ex.setFixedWidth(1101)  # = PyQt5.QtCore.QSize(1100, 770)
