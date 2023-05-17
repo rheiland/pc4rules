ebc2dc42a1c81696cbcf0d4433c7a4d1742b216f
Randy Heiland <randy.heiland@gmail.com>
2023-05-17 11:54:53 -0400

    pass nanohub_flag to run_tab

--- bin/rules_tab.py
+++ bin/rules_tab.py
@@ -133,13 +133,12 @@ class MyQLineEdit(QLineEdit):
 
 class Rules(QWidget):
     # def __init__(self, nanohub_flag):
-    def __init__(self, microenv_tab, celldef_tab):
+    def __init__(self, nanohub_flag, microenv_tab, celldef_tab):
         super().__init__()
 
         self.rules_plot = None
 
-        # self.nanohub_flag = nanohub_flag
-        self.nanohub_flag = False
+        self.nanohub_flag = nanohub_flag
         self.absolute_data_dir = None  # set by studio.py, used by nanoHUB
         self.homedir = '.'  # reset in studio.py
 
